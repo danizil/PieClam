4bb848bf899461b80df16528d69d77ffcca7689d
Juan Cervino <jcervino@cluster-login.lids.mit.edu>
2025-12-18 20:46:57 -0500

    added juan sbatch

--- experiments/scripts/link_prediction/hypers_search/directed/cora/runner.sh
+++ experiments/scripts/link_prediction/hypers_search/directed/cora/runner.sh
@@ -1,7 +1,7 @@
-sbatch bigclam_cora_val_wide.sbatch
+sbatch a_bigclam_cora_val_wide.sbatch
 sleep 1
-sbatch ieclam_cora_val_wide.sbatch
+sbatch a_ieclam_cora_val_wide.sbatch
 sleep 1
-sbatch pclam_cora_val_wide.sbatch
+sbatch a_pclam_cora_val_wide.sbatch
 sleep 1
-sbatch pieclam_cora_val_wide.sbatch
+sbatch a_pieclam_cora_val_wide.sbatch
