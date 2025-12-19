c75f0d2e3413c3dcc7353668280ca1bc1a6758be
Juan Cervino <jcervino@cluster-login.lids.mit.edu>
2025-12-18 20:26:01 -0500

    adding the runners

---
+++ experiments/scripts/link_prediction/hypers_search/directed/citeseer/runner.sh
@@ -0,0 +1,7 @@
+sbatch a_bigclam_citeseer_val_wide.sbatch
+sleep 1
+sbatch  a_ieclam_citeseer_val_wide.sbatch
+sleep 1
+sbatch  a_pclam_citeseer_val_wide.sbatch
+sleep 1
+sbatch  a_pieclam_citeseer_val_wide.sbatch
---
+++ experiments/scripts/link_prediction/hypers_search/directed/cora/runner.sh
@@ -0,0 +1,7 @@
+sbatch bigclam_cora_val_wide.sbatch
+sleep 1
+sbatch ieclam_cora_val_wide.sbatch
+sleep 1
+sbatch pclam_cora_val_wide.sbatch
+sleep 1
+sbatch pieclam_cora_val_wide.sbatch
